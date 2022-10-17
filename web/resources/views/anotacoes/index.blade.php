6394715d5fa434e7700806a1d64b3a490cf096bc
evboas <ev.boas@hotmail.com>
2022-10-17 19:41:02 -0300

    Criação da tabela de visualização de anotações - somente com colunas de nome e data de criação

--- web/resources/views/anotacoes/index.blade.php
+++ web/resources/views/anotacoes/index.blade.php
@@ -2,4 +2,19 @@
     <button class="anotacoes__botao">
         <a href="/anotacoes/criar" class="anotacoes__botao-texto">Criar Anotações</a>
     </button>
+
+    <table class="tabela__anotacoes">
+        <thead>
+            <th>Planta</th>
+            <th>Data</th>
+        </thead>
+        <tbody>
+            @foreach ($anotacoes as $anotacao)
+                <tr>
+                    <td class="tabela__dados-nome">{{$anotacao->nome}}</td>
+                    <td class="tabela__dados-data">{{$anotacao->created_at}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-layout>
