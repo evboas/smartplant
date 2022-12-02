f5fcfa552ee951296a04939857b028788ba3ffbc
evboas <ev.boas@hotmail.com>
2022-12-01 22:52:25 -0300

    Correção do estado da planta na pagina de edição

--- web/resources/views/anotacoes/edit.blade.php
+++ web/resources/views/anotacoes/edit.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Editar anotações">
-    <x-anotacoes.form :action="route('anotacoes.update', $anotacao->id)" :plantas="$plantas" :idplanta="$anotacao->planta_id" :nome="$anotacao->nome" :observacoes="$anotacao->observacoes" />
+    <x-anotacoes.form :action="route('anotacoes.update', $anotacao->id)" :plantas="$plantas" :idplanta="$anotacao->planta_id" :nome="$anotacao->nome" :estado="$anotacao->estado" :observacoes="$anotacao->observacoes" />
 </x-layout>
--- web/resources/views/components/anotacoes/form.blade.php
+++ web/resources/views/components/anotacoes/form.blade.php
@@ -25,19 +25,15 @@
     <div class="formulario_container">
         <label for="estado_planta">Estado da Planta:</label>
         <select name="estado_planta">
-            <option value="Regular">Regular</option>
-            <option value="Bom">Bom</option>
-            <option value="Ruim">Ruim</option>
+            <option value="Regular" @isset($estado) @if($estado == "Regular") selected @endif @endisset>Regular</option>
+            <option value="Bom" @isset($estado) @if($estado == "Bom") selected @endif @endisset>Bom</option>
+            <option value="Ruim" @isset($estado) @if($estado == "Ruim") selected @endif @endisset>Ruim</option>
         </select>
     </div>
     
     <div class="formulario_container formulario_container-observacoes">
         <label for="observacoes">Observações:</label>
-        <textarea name="observacoes" id="observacoes_planta" class="observacoes_planta" cols="100" rows="10">
-            @isset($observacoes)
-                {{$observacoes}}
-            @endisset
-        </textarea>
+        <textarea name="observacoes" id="observacoes_planta" class="observacoes_planta" cols="100" rows="10">@isset($observacoes){{$observacoes}}@endisset</textarea>
     </div>
 
     <button type="submit" class="formulario__botao-anotacoes">Gravar</button>
