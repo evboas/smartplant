886ba9503c8dd1f92352c7dbaefbe4db297c48f0
evboas <ev.boas@hotmail.com>
2022-12-01 23:09:02 -0300

    Adição do parametro :update=false no formulario de criação de anotações

--- web/resources/views/anotacoes/create.blade.php
+++ web/resources/views/anotacoes/create.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Criar anotações">
-    <x-anotacoes.form :action="route('anotacoes.store')" :plantas="$plantas" :nome="old('$planta->nome')" :observacoes="old('observacoes')" />
+    <x-anotacoes.form :action="route('anotacoes.store')" :plantas="$plantas" :nome="old('$planta->nome')" :observacoes="old('observacoes')" :update="false"/>
 </x-layout>
